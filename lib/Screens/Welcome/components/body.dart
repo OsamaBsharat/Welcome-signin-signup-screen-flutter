f64db9e2dd8e419a5bbdc667b1c2d08a2a3e309b
Osama Bsharat <bsharat1999@gmail.com>
2024-03-10 14:34:11 +0200

    welcome-commit

--- lib/Screens/Welcome/components/body.dart
+++ lib/Screens/Welcome/components/body.dart
@@ -1,4 +1,9 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:welcome_signin_signup/Screens/Welcome/components/background.dart';
 
 class Body extends StatelessWidget {
   const Body({super.key});
@@ -6,11 +11,26 @@ class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return Container(
-      height: size.height,
-      width: double.infinity,
-      child: Stack(
-        children: <Image>[],
+    return Background(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            "Welcome Again",
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          SvgPicture.asset(
+            "assests/icons/chat.svg",
+            height: size.height * 0.45,
+          ),
+          TextButton(
+            onPressed: () {},
+            child: Text("Login"),
+          ),
+        ],
       ),
     );
   }
