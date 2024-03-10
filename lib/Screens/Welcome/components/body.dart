338f81cdbe3e634f3a98b60980527c6358d3858c
Osama Bsharat <bsharat1999@gmail.com>
2024-03-10 16:39:58 +0200

    welcome-update-commit

--- lib/Screens/Welcome/components/body.dart
+++ lib/Screens/Welcome/components/body.dart
@@ -1,9 +1,9 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:welcome_signin_signup/Screens/Welcome/components/background.dart';
+import 'package:welcome_signin_signup/components/rounded_button.dart';
+import 'package:welcome_signin_signup/constants.dart';
 
 class Body extends StatelessWidget {
   const Body({super.key});
@@ -12,25 +12,38 @@ class Body extends StatelessWidget {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Background(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text(
-            "Welcome Again",
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.w600,
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              "Welcome Again",
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w600,
+              ),
             ),
-          ),
-          SvgPicture.asset(
-            "assests/icons/chat.svg",
-            height: size.height * 0.45,
-          ),
-          TextButton(
-            onPressed: () {},
-            child: Text("Login"),
-          ),
-        ],
+            SizedBox(
+              height: size.height * 0.03,
+            ),
+            SvgPicture.asset(
+              "assests/icons/chat.svg",
+              height: size.height * 0.45,
+            ),
+            SizedBox(
+              height: size.height * 0.05,
+            ),
+            RoundedButton(
+              text: "LOGIN",
+              press: () {},
+            ),
+            RoundedButton(
+                text: "SIGNUP",
+                color: primaryLightColor,
+                textColor: Colors.black,
+                press: () {}),
+          ],
+        ),
       ),
     );
   }
